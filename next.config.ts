98e1242d1f58c302089a42a7427d8b5c2500df5d
justin.baur <silverhat@live.com>
2026-05-17 13:19:56 -0500

    fix: image hosting and retrieval

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,12 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
   },
   reactStrictMode: true,
 };
